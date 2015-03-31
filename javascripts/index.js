f06fbdf0e3dbb4ad1d0ae6413b5d644251b9d93d
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2015-03-31 09:43:01 -0300

    Include logo.js into the page

---
+++ javascripts/index.js
@@ -0,0 +1,33 @@
+(function() {
+  'use strict';
+
+  // render the default form of the logo
+  var logo = new LogoMagnet(document.getElementById('logo'));
+  logo.render();
+
+  // render only the mantaray
+  var logoManta = new LogoMagnet(document.getElementById('logo-manta'));
+  logoManta.render({
+    backgroundEnabled: false,
+    textEnabled: false
+  });
+
+  // render only the mantaray
+  var logoText = new LogoMagnet(document.getElementById('logo-text'));
+
+  logoText.render({
+    mantaEnabled: false,
+    backgroundEnabled: false
+  });
+
+  window.logoText = logoText;
+
+  // render only the mantaray
+  var logoAnimated = new LogoMagnet(document.getElementById('logo-animation'));
+  logoAnimated.render({
+    backgroundColor: '#00ff00',
+    mantaColor: '#ff0000',
+    textColor: '#0000ff'
+  });
+  logoAnimated.animate();
+}());
